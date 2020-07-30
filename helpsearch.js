084dd4a25215dbd1bf66f22cc8b6e4b8702a3d73
sulthanakhan <sulthanakhan9866@gmail.com>
2020-07-30 21:51:18 +0530

    Add files via upload

--- helpsearch.js
+++ helpsearch.js
@@ -1,125 +1,140 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { forwardRef } from 'react';
+import Avatar from 'react-avatar';
+import Grid from '@material-ui/core/Grid'
+
+import MaterialTable from "material-table";
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 import axios from 'axios'
-import TextField from '@material-ui/core/TextField';
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import FormControl from '@material-ui/core/FormControl'
-export default class Events extends React.Component {
+import Alert from '@material-ui/lab/Alert';
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      events: [],
-      email: '',
-      status:''
-    }
-    this.rows = [{
-      id: 'djnjs',
-      name: 'nvnv',
-      rollno: 'ddbs',
-      status: 'dncsjd'
-    }
-    ]
-    this.search = this.search.bind(this)
-    this.setStatus = this.setStatus.bind(this)
-  }
-setStatus(row) {
-  row.status = this.state.status
-  row.respone = this.state.res
-  console.log(row)
-  axios.post('http://localhost:9996/helpline/response',row)
-      .then(function (response) {
-        this.setState({
-          events: response.data
-        })
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-}
-  search() {
-    axios.post('http://localhost:9996/get/helpline', this.state.email)
-      .then(function (response) {
-        this.setState({
-          events: response.data
-        })
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-  render() {
+const tableIcons = {
+  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
-    return <div>
-      <form noValidate autoComplete="off">
-        <TextField id="standard-basic" label="Search" placeholder="search" style={{ textAlign: 'center' }} onChange={(e) => this.setState({ email: e.target.value })} />
-      </form>
-      <Button onClick={this.search} style={{ textAlign: 'center' }}>Search</Button>
-       <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Id</TableCell>
-              <TableCell align="right">Name</TableCell>
-              <TableCell align="right">Email Id</TableCell>
-              <TableCell align="right">College Name</TableCell>
-              <TableCell align="right">Roll NUmber</TableCell>
-              <TableCell align="right">Pass Out Year</TableCell>
-              <TableCell align="right">HelpLine Subject</TableCell>
-              <TableCell align="right">HelpLine Description</TableCell>
-              <TableCell align="right">Status</TableCell>
-              <TableCell align="right">reponse</TableCell>
-              <TableCell align="right">Filepath</TableCell>
-              <TableCell align="right">Save</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {this.state.events.map((row) => (
-              <TableRow key={row.id}>
-                <TableCell component="th" scope="row">
-                  {row.id}
-                </TableCell>
-                <TableCell align="right">{row.name}</TableCell>
-                <TableCell align="right">{row.emailid}</TableCell>
-                <TableCell align="right">{row.collegename}</TableCell>
-                <TableCell align="right">{row.rollno}</TableCell>
-                <TableCell align="right">{row.passoutyear}</TableCell>
-                <TableCell align="right">{row.helplinesubject}</TableCell>
-                <TableCell align="right">{row.helplinedescription}</TableCell>
-                <TableCell align="right"> <FormControl fullWidth>
-                  <InputLabel id="demo-simple-select-label">status</InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={this.state.status}
-                    onChange={e => this.setState({status:e.target.value})}
-                    fullWidth
-                  >
-                    <MenuItem value="open" name="open">open</MenuItem>
-                    <MenuItem value="close" name="close">close</MenuItem>
-                    
-                  </Select>
-                </FormControl></TableCell>
-                <TableCell align="right"> <TextField onChange={(e) =>this.setState({res:e.target.value})}>{row.response}</TextField></TableCell>
-                <TableCell align="right">{row.filepath}</TableCell>
-                <TableCell align="right"><Button onClick={() => this.setStatus(row)}>Save</Button></TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+const api = axios.create({
+  baseURL: `http://localhost:9993/`
+})
+
+function HelpLineSearch() {
+
+  var columns = [
+    {title: "id", field: "id", hidden: true},
+    {title: "Name", field: "name"},
+    {title: "College Name", field: "emailid"},
+    {title: "Rollnumber", field: "rollnumber"},
+    {title: "PassOutYear", field: "passoutyear"},
+    {title: "HelpLine Subject", field: "helplinesubject"},
+    {title: "HelpLineDescription", field: "helplinedescription"},
+    {title: "status", field: "status",
+    lookup: { 'open': 'Open', 'closed': 'Closed' }},
+    {title: "response", field: "response"},
+    {title: "filepath", field: "filepath"},
+
+  ]
+  const [data, setData] = useState([]); //table data
+
+  const [iserror, setIserror] = useState(false)
+  const [errorMessages, setErrorMessages] = useState([])
+
+  useEffect(() => { 
+    api.get("/users")
+        .then(res => {               
+            setData(res.data.data)
+            console.log(res.data.data)
+         })
+         .catch(error=>{
+             console.log("Error")
+         })
+  }, [])
+
+  const handleRowUpdate = (newData, oldData, resolve) => {
+    //validation
+    
+   console.log(newData)
+      api.post("/users/", newData)
+      .then(res => {
+        const dataUpdate = [...data];
+        const index = oldData.tableData.id;
+        dataUpdate[index] = newData;
+        setData([...dataUpdate]);
+        resolve()
+        console.log(dataUpdate)
+        console.log(data)
+      })
+      .catch(error => {
+        setErrorMessages(["Update failed! Server error"])
+        setIserror(true)
+        resolve()
+        
+      })
+     
+    
+  }
+
+  return (
+    <div className="App">
+      
+      <Grid container spacing={1}>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={10}>
+          <div>
+            {iserror && 
+              <Alert severity="error">
+                  {errorMessages.map((msg, i) => {
+                      return <div key={i}>{msg}</div>
+                  })}
+              </Alert>
+            }       
+          </div>
+            <MaterialTable
+              title="User data from remote source"
+              columns={columns}
+              data={data}
+              icons={tableIcons}
+              editable={{
+                onRowUpdate: (newData, oldData) =>
+                  new Promise((resolve) => {
+                      handleRowUpdate(newData, oldData, resolve);
+                }),
+              }}
+            />
+          </Grid>
+          <Grid item xs={3}></Grid>
+        </Grid>
     </div>
-
-  }
+  );
 }
+
+export default HelpLineSearch;
